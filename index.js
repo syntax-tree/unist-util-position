64062815867a8c10adbd22e47683377c23a024fc
Titus Wormer <tituswormer@gmail.com>
2019-05-22 10:28:57 +0200

    Remove superfluous comments

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 'use strict'
 
-/* Expose. */
 var position = exports
 
 position.start = factory('start')
 position.end = factory('end')
 
-/* Factory to get a `type` point in the positional info of a node. */
 function factory(type) {
   point.displayName = type
 
   return point
 
-  /* Get a point in `node.position` at a bound `type`. */
   function point(node) {
     var point = (node && node.position && node.position[type]) || {}
 
