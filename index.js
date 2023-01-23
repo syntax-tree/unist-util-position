744c0a848817bee8b9daddc6e817695d73d5011b
Nathan Bierema <nbierema@gmail.com>
2023-01-22 22:21:20 -0500

    Export types from main entrypoint

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+/**
+ * @typedef {import('./lib/index.js').NodeLike} NodeLike
+ * @typedef {import('./lib/index.js').PositionLike} PositionLike
+ * @typedef {import('./lib/index.js').PointLike} PointLike
+ */
+
 export {pointEnd, pointStart, position} from './lib/index.js'
